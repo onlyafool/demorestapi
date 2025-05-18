5e07b69c35840a13315a02a754ed9dc9bcb8eb54
Ilona Fischer <onlyafool@gmx.net>
2025-05-18 18:32:40 +0200

    feat: CORS-Unterstützung und Bereitstellung statischer Dateien hinzugefügt; cors-Middleware für Cross-Origin-Anfragen integriert; express.static für das Bereitstellen von Dateien aus dem public-Ordner; package.json und package-lock.json entsprechend angepasst; kleinere Anpassungen in app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import usersRouter from './routes/users.js';
 import eventRouter from './routes/events.js';
-import db from './database.js';
+import db from './database.js'; 
+
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use('/users', usersRouter);
 app.use('/events', eventRouter);
